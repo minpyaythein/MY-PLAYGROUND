6a8af03f9d90e5c1695e772ad07caa4b17016610
minpyaythein <paukking@gmail.com>
2024-02-15 23:11:40 +0900

    changing node version to 21.6.1

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+﻿#!/bin/bash
 #launching container
 
 echo "building image!"
 docker build -t my-playground .
 echo "build completed!"
 echo "now, starting container!"
-docker run -d -p 8080:5173 -v "$(pwd)":/app -v /app/node_modules --rm my-playground 
+docker run -d -p 8080:5173 -v "$(pwd)":/app -v /app/node_modules --rm my-playground
 echo "container launched! let's roll!"
