04e7e3fce10504f3018d83b4edcd0d88c4baf1fb
minpyaythein <paukking@gmail.com>
2024-02-18 22:56:41 +0900

    preparing backend

---
+++ backend/src/main/java/backend/myplayground/MyplaygroundApplication.java
@@ -0,0 +1,13 @@
+package backend.myplayground.myplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplaygroundApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/myplayground/myplayground/MyplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.myplayground.myplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
